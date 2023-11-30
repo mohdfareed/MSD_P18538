5906a66a9f602a36aab16f1a06d6ae747da936d6
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-11-30 15:29:14 -0500

    Refactor transcription

--- software/control/audio/transcription.py
+++ software/control/audio/transcription.py
@@ -4,62 +4,102 @@ from time import sleep
 from typing import Any, Callable, Generator
 
 import speech_recognition as sr
-from audio import core as audio
-from audio import engines, logger
+from audio import logger
+
+# globals
+recorder: sr.Recognizer = sr.Recognizer()
+"""The speech recognition engine."""
 
 # constants
 PHRASE_TERMINATOR = "\n"
 """The indicator of an end of a phrase."""
+_PHRASE_TIMEOUT = 1.5  #  the maximum pause between phrases (seconds)
+_RECORD_TIMEOUT = 0.5  # the maximum recording chunk size (seconds)
+_MAX_RECORD_DURATION = 5  # the maximum recording duration (seconds)
 
-_phrase_timeout = 2.5  #  the maximum pause between phrases (seconds)
+# configure the recorder
+recorder.energy_threshold = 1000
+recorder.dynamic_energy_threshold = False
 
 
 def transcribe(
-    recordings: Queue[bytes] = audio.recordings,
-    sample_rate: int = audio.source.SAMPLE_RATE,
-    sample_width: int = audio.source.SAMPLE_WIDTH,
-    recognize: Callable[[sr.AudioData], str] = engines.google_recognize,
+    source: sr.AudioSource, recognizer: Callable[[sr.AudioData], str]
 ) -> Generator[str, Any, None]:
-    """
-    Transcribe audio in a recording queue using a recognition engine
-    progressively. Yields the transcription of the audio, separated by
-    phrases.
+    """Generate transcription of audio from a source using a recognition engine
+    progressively. Yields the transcription of the audio, separated as phrases.
 
     Args:
-        recordings (Queue): The queue of recorded audio chunks.
-        recognize (AudioData -> str): The recognition engine. Defaults to
+        source (AudioSource): The audio source to record from.
+        recognizer (AudioData -> str): The recognition engine. Defaults to
         Google's speech recognition engine.
 
     Yields:
-        str: The transcription of the audio. PHRASE_TERMINATOR indicates the
-        start of a new phrase.
+        str: The transcription of the audio in the provided queue.
+        PHRASE_TERMINATOR indicates the start of a new phrase.
+        None: If the audio could not be recognized.
     """
 
-    phrase_time: datetime = datetime.min  # last time new audio was received
+    recordings = _record(source)  # start listening in the background
+    phrase_time = datetime.min  # last time new audio was received
     phrase_buffer = bytes()  # buffer of current (incomplete) phrase audio
+    sample_rate = 0  # the sample rate of the audio
+    sample_width = 0  # the sample width of the audio
 
     while True:
         if recordings.empty():
+            sleep(0.1)  # reduce cpu usage
             continue  # wait for new audio data
 
         # check for a new phrase (pause in speech)
         now = datetime.utcnow()
-        if now - phrase_time > timedelta(seconds=_phrase_timeout):
+        if now - phrase_time > timedelta(seconds=_PHRASE_TIMEOUT):
             phrase_buffer = bytes()
             yield PHRASE_TERMINATOR  # indicate a pause between phrases
-        phrase_time = now
+        phrase_time = now  # update last time new audio was received
 
         # build phrase audio data
         while not recordings.empty():
-            data = recordings.get()
-            phrase_buffer += data
+            audio_data = recordings.get()  # read from the queue
+            # update sample rate and width
+            if not sample_rate or not sample_width:
+                sample_rate = audio_data.sample_rate
+                sample_width = audio_data.sample_width
+            # add audio data to buffer
+            phrase_buffer += audio_data.get_raw_data()
+            if len(phrase_buffer) > sample_rate * _MAX_RECORD_DURATION:
+                phrase_time = datetime.min  # start a new phrase
+                break  # stop if the buffer is too large
         audio_data = sr.AudioData(phrase_buffer, sample_rate, sample_width)
 
         try:  # transcribe the audio
-            yield recognize(audio_data)
+            yield recognizer(audio_data)
         except sr.UnknownValueError:
             continue  # unrecognized audio
         except sr.RequestError as e:
             logger.error("Transcription failed: %s", e)
             continue  # recognition engine error
         sleep(0)  # important for multithreading
+
+
+def _record(source: sr.AudioSource = sr.Microphone(sample_rate=16000)):
+    """Start recording audio in the background.
+
+    Args:
+        source (AudioSource): The audio source to record from.
+
+    Returns:
+        Queue: The queue of recorded audio chunks. The chunks are raw audio
+        data. The queue is thread-safe.
+    """
+    recordings: Queue[sr.AudioData] = Queue()
+
+    def callback(_, audio: sr.AudioData) -> None:
+        recordings.put(audio)
+
+    with source:
+        recorder.adjust_for_ambient_noise(source)
+    recorder.listen_in_background(
+        source, callback, phrase_time_limit=_RECORD_TIMEOUT
+    )
+
+    return recordings
--- software/control/main.py
+++ software/control/main.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 import os
+import threading
 
-from audio.core import record as record_audio
-from audio.transcription import PHRASE_TERMINATOR, transcribe
+from audio import engines as recognition_engines
+from audio import transcription
 from controller import bluetooth_controller as controller
 from robot import motor, servo, speaker
 
 
 def main():
-    record_audio()
-    transcriptions = [""]
-    for transcript in transcribe():
-        if transcript == PHRASE_TERMINATOR:
-            transcriptions.append("")  # start new phrase
-        transcriptions[-1] = transcript
-        print_transcription(transcriptions)
+    # controller.start()
+    test_transcription()
+    # test_motors()
+    # test_speakers()
 
-    controller.start()
-    test_motors()
-    test_speakers()
+    # wait for the user to exit
+    input("Press anything to exit.\n")
+    exit(0)
 
 
 def test_motors():
@@ -32,12 +30,26 @@ def test_speakers():
         speaker.play(sound)
 
 
-def print_transcription(transcription: list[str]):
-    os.system("cls" if os.name == "nt" else "clear")
-    print("Transcription:")
-    for line in transcription:
-        print(line)
-    print("", end="", flush=True)
+def test_transcription():
+    # source = transcription.sr.Microphone(sample_rate=16000)
+    source = transcription.sr.AudioFile("demo.wav")
+    recognizer = recognition_engines.whisper_recognize
+
+    # function to display transcriptions
+    def print_transcription(transcription: str):
+        # clear line and print transcription
+        print(" " * os.get_terminal_size().columns, end="\r", flush=True)
+        print(transcription, end="\r", flush=True)
+
+    def transcribe():  # transcribe audio from a source in real-time
+        print("Listening...")
+        for transcript in transcription.transcribe(source, recognizer):
+            if transcript == transcription.PHRASE_TERMINATOR:
+                continue
+            print_transcription(transcript)
+
+    # start transcribing in the background
+    threading.Thread(target=transcribe, daemon=True).start()
 
 
 if __name__ == "__main__":
