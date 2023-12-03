0f8fd505279204c1c22d910ee3afd718c9b5102d
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-12-03 17:33:32 -0500

    Fix streaming bugs

--- software/frontend/Services/TranscriptionService.cs
+++ software/frontend/Services/TranscriptionService.cs
@@ -1,3 +1,7 @@
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
 namespace Services;
 
 public class TranscriptionService
@@ -11,25 +15,27 @@ public class TranscriptionService
         _route = globalSettings.BackendBaseAddress + "/transcription/";
     }
 
-    public async IAsyncEnumerable<string> GetTranscriptionStreamAsync()
+    public async IAsyncEnumerable<string> GetTranscriptionStreamAsync(
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        using var response = await _httpClient.GetStreamAsync(_route);
-        using var reader = new StreamReader(response);
-        while (!reader.EndOfStream)
+        // Configure the request
+        using var request = new HttpRequestMessage(HttpMethod.Get, _route);
+        request.SetBrowserResponseStreamingEnabled(true);
+
+        // Send the request and get the response
+        using var response = await _httpClient.SendAsync(request,
+        HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+        // Read the response line by line
+        var bytes = new byte[1024]; // max line length
+        var bytesCount = 0;
+        while ((bytesCount = await stream.ReadAsync(bytes, cancellationToken)) > 0)
         {
-            Console.WriteLine("Reading line");
-            var line = await reader.ReadLineAsync();
-            if (!string.IsNullOrEmpty(line))
-            {
-                yield return line;
-            }
+            yield return Encoding.UTF8.GetString(bytes, 0, bytesCount);
+            await Task.Delay(100, cancellationToken); // delay to avoid blocking
+            cancellationToken.ThrowIfCancellationRequested();
+            // FIXME: close the stream when the user stops the transcription
         }
-
-    }
-
-    public async Task StopTranscriptionAsync()
-    {
-        var response = await _httpClient.DeleteAsync(_route);
-        response.EnsureSuccessStatusCode();
     }
 }
