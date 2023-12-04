763f0c51497e434c461e8b2be1c0b1a310570ff7
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-12-03 21:36:28 -0500

    Rework cancellation and add minor visual fixes

--- software/backend/app/controllers/transcription.py
+++ software/backend/app/controllers/transcription.py
@@ -8,18 +8,43 @@ from ..services.transcription import core as transcription
 from ..services.transcription import engines as recognition_engines
 
 router = APIRouter()
+_transcriber = None  # active transcription generator
+_cancellation_token = asyncio.Event()
 
 
 @router.get("/transcription/", status_code=status.HTTP_200_OK)
-async def get_transcription():
+async def start_transcription():
+    global _transcriber
+    if _transcriber is not None:
+        raise HTTPException(
+            status_code=400, detail="Transcription already running"
+        )
+
     source = transcription.sr.Microphone(sample_rate=16000)
     recognizer = recognition_engines.whisper_recognize
+    _transcriber = transcription.transcribe(source, recognizer)
+    _cancellation_token.clear()
 
     async def _transcription():
         try:
-            async for text in transcription.transcribe(source, recognizer):
-                yield text
+            async for transcript in _transcriber:  # type: ignore
+                yield transcript
+                if _cancellation_token.is_set():
+                    break
         except asyncio.CancelledError or GeneratorExit or KeyboardInterrupt:
             LOGGER.debug("Transcription interrupted")
+        finally:
+            LOGGER.debug("Transcription stopped")
+            _transcriber = None
 
     return StreamingResponse(_transcription())
+
+
+@router.delete("/transcription/", status_code=status.HTTP_200_OK)
+async def stop_transcription():
+    global _transcriber
+    if _transcriber is None:
+        raise HTTPException(
+            status_code=400, detail="Transcription not running"
+        )
+    _cancellation_token.set()
--- software/frontend/Services/TranscriptionService.cs
+++ software/frontend/Services/TranscriptionService.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
@@ -15,8 +14,7 @@ public class TranscriptionService
         _route = globalSettings.BackendBaseAddress + "/transcription/";
     }
 
-    public async IAsyncEnumerable<string> GetTranscriptionStreamAsync(
-        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    public async IAsyncEnumerable<string> GetTranscriptionStreamAsync()
     {
         // Configure the request
         using var request = new HttpRequestMessage(HttpMethod.Get, _route);
@@ -24,18 +22,22 @@ public class TranscriptionService
 
         // Send the request and get the response
         using var response = await _httpClient.SendAsync(request,
-        HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+        HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+        using var stream = await response.Content.ReadAsStreamAsync();
 
         // Read the response line by line
         var bytes = new byte[1024]; // max line length
         var bytesCount = 0;
-        while ((bytesCount = await stream.ReadAsync(bytes, cancellationToken)) > 0)
+        while ((bytesCount = await stream.ReadAsync(bytes)) > 0)
         {
             yield return Encoding.UTF8.GetString(bytes, 0, bytesCount);
-            await Task.Delay(100, cancellationToken); // delay to avoid blocking
-            cancellationToken.ThrowIfCancellationRequested();
-            // FIXME: close the stream when the user stops the transcription
+            await Task.Delay(1); // delay to avoid blocking the UI
         }
+    }
+
+    public async Task StopTranscriptionAsync(CancellationToken cancellationToken = default)
+    {
+        await _httpClient.DeleteAsync(_route, cancellationToken);
     }
 }
