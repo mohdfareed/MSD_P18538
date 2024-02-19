48613c8ab3fe1adecc6307538f7c37ffbd71616e
BrianMonclus <brianmonclus@gmail.com>
2024-02-19 13:33:31 -0500

    Make a couple of changes to the setup script

--- setup.sh
+++ setup.sh
@@ -4,13 +4,18 @@
 #  - Eventally might need to ensure that certain directories get created
 #
 
+# Ensure root privileges 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 # Update all packages
 apt update && apt upgrade
 
 # For adhoc network ensure we have dhcp server downloaded and configured to automaticly handle Ip hosting
 apt install isc-dhcp-server
-cat ~/MSD_P18538/software/backend/app/services/network/dhcpd.conf >> /etc/dhcp/chcpd.conf
+cp ~/MSD_P18538/software/backend/app/services/network/dhcpd.conf /etc/dhcp/dhcpd.conf
 
 # Install Python 3.12.1 and set as the primary version 
 apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev  libsqlite3-dev
@@ -36,7 +41,7 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version lates
 
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
-source ~/.bashrc
+source ~/.bashrc # Warning: This line failed, might have to manually run it
 
 # Check the installs
 python -VV
