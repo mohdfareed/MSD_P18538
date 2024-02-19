972219bff3c544484fe1588f4308643a6fac0961
BrianMonclus <brianmonclus@gmail.com>
2024-02-19 14:44:10 -0500

    Plan out the motor control pin mapping based on weelye diagram

--- software/backend/app/services/control/motors.py
+++ software/backend/app/services/control/motors.py
@@ -3,11 +3,21 @@ from gpiozero import Motor, Servo
 from ...models.movement import Movement
 from . import LOGGER
 
-MOTOR_LEFT_FORWARD = 17
-MOTOR_LEFT_BACKWARD = 27
-MOTOR_RIGHT_FORWARD = 4
-MOTOR_RIGHT_BACKWARD = 14
-SERVO = 18
+# These pins could be a good choice for this purpose
+# GPIO  -> PIN
+# 0 -> 27 note: might be reserved
+# 5 -> 29
+# 6 -> 31
+# 13 -> 33, also PWM1
+# 19 -> 35, also PWM1
+# 26 -> 37 
+
+
+# For now we are going to test without PWM, not sure for now the switching speed for the ICs 
+MOTOR_SPEED = 6 # High is fast, and low is slow. Not sure if this matters for for reverse..
+MOTOR_FORWARD = 13
+MOTOR_REVERSE = 19 
+STEERING_MOTOR = 18 # Should be noted that this not a servo, but rather just another 
 
 # servo = Servo(SERVO)
 # motor_left = Motor(
