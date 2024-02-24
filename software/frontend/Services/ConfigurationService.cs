80e84a35c346b0f9250e5f8954bd38fb8ddaf254
Mohammed Fareed <mohdf.fareed@icloud.com>
2024-02-23 21:34:50 -0500

    Add frontend config page

---
+++ software/frontend/Services/ConfigurationService.cs
@@ -0,0 +1,64 @@
+using System.Text.Json;
+using Microsoft.JSInterop;
+
+namespace Services;
+
+public class ConfigurationService
+{
+    const string _route = "/config/"; // API route
+
+    // dependencies
+    private readonly DotNetObjectReference<ConfigurationService> _objRef;
+    private readonly HttpClient _httpClient; // used to call API
+    private readonly ILogger<ConfigurationService> _logger;
+
+    // routes
+    private readonly string _http_route; // API http route
+
+
+    public ConfigurationService(HttpClient httpClient,
+    Models.GlobalSettings globalSettings, ILogger<ConfigurationService> logger)
+    {
+        _httpClient = httpClient;
+        _http_route = globalSettings.BackendHTTPUrl + _route;
+
+        _objRef = DotNetObjectReference.Create(this);
+        _logger = logger;
+    }
+
+    public async Task<Models.Config> GetConfigAsync()
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync(_http_route);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var config = JsonSerializer.Deserialize<Models.Config>(responseString);
+            return config!;
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex, "Failed to get configuration");
+            throw;
+        }
+    }
+
+    public async Task SetConfigAsync(string key, string value)
+    {
+        try
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                { "value", value }
+            };
+            var response = await _httpClient.PostAsync(_http_route + key,
+            new FormUrlEncodedContent(parameters));
+            response.EnsureSuccessStatusCode();
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex, "Failed to set configuration");
+            throw;
+        }
+    }
+}
