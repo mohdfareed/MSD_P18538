6b35644bf994436f46e2367556f8c2cd980e1e2a
BrianMonclus <brianmonclus@gmail.com>
2024-02-27 22:44:24 -0500

    Add bluetooth and wifi to the configuration page

--- software/frontend/Models/Configuration.cs
+++ software/frontend/Models/Configuration.cs
@@ -6,4 +6,6 @@ public class Config
 {
     [JsonPropertyName("transcription_engine")]
     public string TranscriptionEngine { get; set; } = null!;
+    public bool BluetoothOn { get; set; } = false;
+    public bool AdhocOn { get; set; } = false;
 }
